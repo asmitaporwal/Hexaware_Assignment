5e1547d2b9d57bb744cb48854a4582a24a19c257
Asmita Porwal <asmita.porwal04@gmail.com>
2023-12-26 00:25:38 +0530

    Assignment-1(Techshop)

--- Assignment/python/Assignment-1/TechShop/DBproperty.py
+++
@@ -1,7 +0,0 @@
-class PropertyUtil:
-    def getPropertyString():
-        host = 'localhost'
-        username = 'root'
-        password = 'Root'
-        database = 'techshop'
-        return host, username, password, database
--- Assignment/python/Assignment-1/TechShop/Inventory.py
+++
@@ -1,8 +0,0 @@
-class Inventory:
-    def __init__(self,inventory_id,product,quantity_in_stock,last_stock_update):
-        self.InventoryID=inventory_id
-        self.Product=product
-        self.QuantityInStock=quantity_in_stock
-        self.LastStockUpdate=last_stock_update
-
-    
