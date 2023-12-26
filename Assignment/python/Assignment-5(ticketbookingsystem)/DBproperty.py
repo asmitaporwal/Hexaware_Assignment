ef6c995a63718a44fdef27f771081f7efbb4ad40
Asmita Porwal <asmita.porwal04@gmail.com>
2023-12-27 02:57:03 +0530

    Assignment-2(hmbank)

--- Assignment/python/Assignment-5(ticketbookingsystem)/DBproperty.py
+++
@@ -1,7 +0,0 @@
-class PropUtil:
-    def getPropertyString():
-        host = 'localhost'
-        username = 'root'
-        password = 'Root'
-        database = 'ticketbookingsystem'
-        return host, username, password, database
--- Assignment/python/Assignment-5(ticketbookingsystem)/exceptions.py
+++
@@ -1,9 +0,0 @@
-class EventNotFoundException(Exception):
-    def __init__(self, message="EventNotFoundException"):
-        self.message = message
-        super().__init__(self.message)
-
-class InvalidBookingIDException(Exception):
-    def __init__(self, message="InvalidBookingIDException"):
-        self.message = message
-        super().__init__(self.message)
